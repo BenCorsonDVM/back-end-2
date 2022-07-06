7154f173c2af690203d78cefb3e16eaa257a5fe9
Ben Corson <corsonbenjamin@gmail.com>
2022-07-06 16:06:32 -0500

    back-end-2

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+//import our controller function
+const {getHouses, createHouse, updateHouse, deleteHouse} = require('./controller')
+
+//endpoints
+app.get('/api/houses', getHouses)
+app.post('/api/houses', createHouse)
+app.put('/api/houses/:id', updateHouse)
+app.delete('/api/houses/:id', deleteHouse)
+
+
+
+app.listen(4004, () => console.log('Server running on 4004'))
